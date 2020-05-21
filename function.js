7a1d00f1ffff349ab6ce39beab8fe140c5bb7195
Ayako Shiraishi <ayako3088@Ayakos-MacBook-Air.local>
2020-05-21 09:04:39 -0400

    fix a little

--- function.js
+++ function.js
@@ -1,22 +1,43 @@
 
+let todos = [];
     //ユーザー入力値ゲット
-    const add = document.getElementById("btn-add");　//追加ボタンのID取得 → 入力値を得るため
-    const table = document.getElementById("insert")　//table のID取得　→ 
-    
-    add.addEventListener("click", () => {
-        let contents = document.getElementById("input").value; //追加ボタンを押す度に入力値を取得
-        let rowLen = table.rows.length // IDの番号のため、table の列の長さを取得
-        let newRow = table.insertRow(-1); //末に行を追加する　0だと最初の行に追加
-        let cell1 = newRow.insertCell(0);　//追加した行にセル追加 0で一番初めのセル
-        let cell2 = newRow.insertCell(1);
-        let cell3 = newRow.insertCell(2);
-        let cell4 = newRow.insertCell(3);
+const add = document.getElementById("btn-add");　//追加ボタンのID取得 → 入力値を得るため
+const table = document.getElementById("insert")　//table のID取得　→ 
+
+const displayTodos = (task) => { //task を渡す
+    comment = document.getElementById("input").value; //入力値を取得
+    const todo = { //オブジェクト作成、格納
+        task : "",
+        status: "作業中"
+    }
+    todos.push(todo); 
+    console.log(todo);
+}
+
+
+
+add.addEventListener("click", () => {
+         displayTodos();
+        const rowLen = table.rows.length // IDの番号のため、table の列の長さを取得
+        const newRow = table.insertRow(-1); //末に行を追加する　0だと最初の行に追加
+        const cellForId = newRow.insertCell(0);　//追加した行にセル追加 0で一番初めのセル
+        const cellForComment = newRow.insertCell(1);
+        const cellForWorking = newRow.insertCell(2);
+        const cellForDelete = newRow.insertCell(3);
         let id = document.createTextNode(rowLen - 1); //document.createTextNode   
-        let comment = document.createTextNode(contents); //セルに入れるため、入力データを文字に
-        const workingBtn = '<input type="button" value="作業中" />'; //htmlを文字で
-        const deleteBtn = '<input type="button" value="削除" />';
-        cell1.appendChild(id); //cell1にデータを入れる。
-        cell2.appendChild(comment);
-        cell3.innerHTML = workingBtn;
-        cell4.innerHTML = deleteBtn;
+        //let comment = document.createTextNode(task); //セルに入れるため、入力データを文字に
+        //document.createElement(tagName[, options]);
+        //ボタンにクリックイベントを追加する時など、扱いやすくなる。
+        const workingBtn = document.createElement("input");
+        workingBtn.type = "button"; // workingBtn.type = string →　input type設定
+        workingBtn.value = "作業中";  //input value設定
+
+        const deleteBtn = document.createElement("input");
+        deleteBtn.type = "button";
+        deleteBtn.value = "削除";
+
+        cellForId.appendChild(id); //cellForidにデータを入れる。DOM操作：appendChild
+        cellForComment.appendChild();
+        cellForWorking.appendChild(workingBtn);
+        cellForDelete.appendChild(deleteBtn);
     });
