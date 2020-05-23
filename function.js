9eed54662dbb3b5a2a124dc52dcd3b14920025fd
Ayako Shiraishi <ayako3088@Ayakos-MacBook-Air.local>
2020-05-23 12:24:41 -0400

    fix some issue

--- function.js
+++ function.js
@@ -1,45 +1,57 @@
 
-let todos = [];
+const todos = [];
 const add = document.getElementById("btn-add");　//追加ボタンのID取得 → 入力値を得るため
-const table = document.getElementById("insert")　//table のID取得
+const table = document.getElementById("insert")　//table のID取得 html内にテーブル作成
 
-const displayTodos = () => {
+add.addEventListener("click", () => {
+    addTodo();
+    displayTodos();
+});
+
+const addTodo = () =>{
     let input = document.getElementById("input").value; //入力値を取得
-    let rowLen = table.rows.length // IDの番号のため、table の列の長さを取得
-    const newRow = table.insertRow(-1); //末に行を追加する　0だと最初の行に追加
-    const cellForId = newRow.insertCell(0);　//追加した行にセル追加 0で一番初めのセル
-    const cellForComment = newRow.insertCell(1);
-    const cellForWorking = newRow.insertCell(2);
-    const cellForDelete = newRow.insertCell(3);
-
     const todo = { //オブジェクト作成、格納
         task: input,
         status: "作業中"
     }
     todos.push(todo);
-    for (let i in todos) {
-        let comment = document.createTextNode(todos[i].task);
-        cellForComment.appendChild(comment);
-    }
+}
 
-    //cellForidにデータを入れる。DOM操作：appendChild
-    let id = document.createTextNode(rowLen - 1);
-    cellForId.appendChild(id);
-
+const createWorkingBtn = () => {
     //document.createElement(tagName[, options]);
     //ボタンにクリックイベントを追加する時など、扱いやすくなる。
     const workingBtn = document.createElement("input");
     workingBtn.type = "button"; // workingBtn.type = string →　input type設定
     workingBtn.value = "作業中";  //input value設定
+    return workingBtn;
+}
 
+const createDeleteBtn = () => {
     const deleteBtn = document.createElement("input");
     deleteBtn.type = "button";
     deleteBtn.value = "削除";
-  
-    cellForWorking.appendChild(workingBtn);
-    cellForDelete.appendChild(deleteBtn);
+    return deleteBtn;
 }
 
-add.addEventListener("click", () => {  
-        displayTodos();
-    });
+const displayTodos = () => {
+    const newRow = table.insertRow(-1); //末に行を追加する　0だと最初の行に追加
+    const cellForId = newRow.insertCell(0);　//追加した行にセル追加 0で一番初めのセル
+    const cellForComment = newRow.insertCell(1);
+    const cellForWorking = newRow.insertCell(2);
+    const cellForDelete = newRow.insertCell(3);
+    Object.keys(todos).forEach((key) => {
+        let comment = document.createTextNode(todos[key].task);    
+        cellForComment.appendChild(comment);
+        console.log(todos);
+    });  
+    //cellForidにデータを入れる。DOM操作：appendChild
+    let id = document.createTextNode(todos.length - 1);
+    cellForId.appendChild(id);
+    cellForWorking.appendChild(createWorkingBtn());
+    cellForDelete.appendChild(createDeleteBtn());
+    
+}
+
+
+
+
