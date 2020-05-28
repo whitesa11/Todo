68bb557e36b7a89d63f6e415691af29e3d6106eb
Ayako Shiraishi <ayako3088@Ayakos-MacBook-Air.local>
2020-05-28 13:15:45 -0400

    delete todo

--- function.js
+++ function.js
@@ -3,12 +3,12 @@ const todos = [];
 const add = document.getElementById("btn-add");　//追加ボタンのID取得 → 入力値を得るため
 const table = document.getElementById("insert")　//table のID取得 html内にテーブル作成
 
-add.addEventListener("click", () => {  
-    addTodo();  
+add.addEventListener("click", () => { 
+    addTodo();
 });
 
-const addTodo = () =>{
-    let input = document.getElementById("input").value; //入力値を取得
+const addTodo = () => {
+    const input = document.getElementById("input").value; //入力値を取得
     document.getElementById("input").value = ""; //テキストボックスを空欄に戻す
     const todo = { //オブジェクト作成、格納
         task: input,
@@ -31,6 +31,7 @@ const createDeleteBtn = () => {　
     const deleteBtn = document.createElement("input");
     deleteBtn.type = "button";
     deleteBtn.value = "削除";
+    deleteBtn.className = "delete";
     return deleteBtn;
 }
 
@@ -41,19 +42,35 @@ const displayTodos = () => {
     const cellForWorking = newRow.insertCell(2);
     const cellForDelete = newRow.insertCell(3);
     todos.forEach(todo => {   
-        let comment = document.createTextNode(todo.task); 
+        const comment = document.createTextNode(todo.task); 
         cellForComment.textContent = null; //一旦前回のセルのテキストをクリア
         cellForComment.appendChild(comment);
-    });
-    
+    });  
     //ボタン追加
     const wBtn = createWorkingBtn();
     const dBtn = createDeleteBtn();
-    let id = document.createTextNode(todos.length - 1);
+    const id = document.createTextNode(todos.length - 1);
     cellForId.appendChild(id);
     cellForWorking.appendChild(wBtn);
     cellForDelete.appendChild(dBtn);
+
+    deleteTodo(); 
 }
+
+const daleteTodoItem = document.getElementsByClassName("delete");
+function deleteTodo(){
+for (let i = 0; i < daleteTodoItem.length; i++) {
+    daleteTodoItem[i].addEventListener('click', deleteListItem, false);
+};
+};
+
+function deleteListItem () {
+    let tr = this.parentNode.parentNode; //押したボタンの行を取得
+    console.log(this.parentNode.parentNode);
+    tr.parentNode.deleteRow(tr.sectionRowIndex); //その行のインデックスを取得して削除
+};
+
+
 
 
 
